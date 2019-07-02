a91ba8e1d78e635d393b530816fe5943a2318ff1
Parinya Samrongsap <zabahey.sukoi@gmail.com>
2019-07-02 22:08:24 +0700

    type basic

--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-console.log("It works!");
+let myName: string = 'Name'
+// myName = 28
+
+// number
+let myAge: number = 33.33
+// myAge = 'Max'
+
+// boolean
+let hasHobiies: boolean = true
+// hasHobiies = 1
+
+// assign types
+let myRealAge: number
+myRealAge = 33
+// myRealAge = '33'
