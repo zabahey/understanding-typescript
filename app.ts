a0faafd19822c9a7e34a22cc0d56dea36befdf2b
Parinya Samrongsap <zabahey.sukoi@gmail.com>
2019-07-02 22:43:59 +0700

    Put it all together as a complex type

--- app.ts
+++ app.ts
@@ -75,3 +75,11 @@ let userData: { name: string; age: number } = {
 // 	a: 'hello',
 // 	b: 22
 // }
+
+// complex object
+let complex: { data: number[]; output: (all: boolean) => number[] } = {
+	data: [100, 3.99, 10],
+	output: function(all: boolean): number[] {
+		return this.data
+	}
+}
