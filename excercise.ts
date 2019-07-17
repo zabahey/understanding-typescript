b34098581d8645c21286e7dbe220d5559ca465a2
Parinya Samrongsap <zabahey.sukoi@gmail.com>
2019-07-17 21:54:08 +0700

    Solving excercise

---
+++ excercise.ts
@@ -0,0 +1,20 @@
+type BankAccount = { money: number; deposit: (value: number) => void }
+
+let bankAccount: BankAccount = {
+	money: 2000,
+	deposit(value) {
+		this.money += value
+	}
+}
+
+type Profile = { name: string; bankAccount: BankAccount; hobbies: string[] }
+
+let myself: Profile = {
+	name: 'Max',
+	bankAccount: bankAccount,
+	hobbies: ['Sports', 'Cooking']
+}
+
+myself.bankAccount.deposit(3000)
+
+console.log(myself)
